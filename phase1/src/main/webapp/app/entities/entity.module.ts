1d971a5e5584a18880cfd6a0ff2e3f039e16e020
MathieuAA <mathieu.aa@free.fr>
2018-06-21 17:32:50 +0200

    Cleaned the first demo

--- phase1/src/main/webapp/app/entities/entity.module.ts
+++ phase1/src/main/webapp/app/entities/entity.module.ts
@@ -1,20 +1,10 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { Phase1CategoryModule } from './category/category.module';
-import { Phase1ProductModule } from './product/product.module';
-import { Phase1CustomerModule } from './customer/customer.module';
-import { Phase1AddressModule } from './address/address.module';
-import { Phase1WishlistModule } from './wishlist/wishlist.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
-        Phase1CategoryModule,
-        Phase1ProductModule,
-        Phase1CustomerModule,
-        Phase1AddressModule,
-        Phase1WishlistModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
